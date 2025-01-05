70926c29ca0b608a940d4740af816ec9ef503004
ozudev <ozudev@gmail.com>
2025-01-05 13:21:50 +0300

    Week 4 uygulama 5 (Inheritance)

---
+++ Week04_05/Program.cs
@@ -0,0 +1,39 @@
+﻿var kisi = new BaseKisi { Ad = "Banu", Soyad = "Yılmaz" };
+kisi.ShowInfo();
+
+var ogrenci = new Ogrenci { Ad = "Oğuzhan", Soyad = "Uğuz", OgrenciNo = 123 };
+ogrenci.ShowInfo();
+
+var ogretmen = new Ogretmen { Ad = "Sertan", Soyad = "Bozkuş", Maas = 5000 };
+ogretmen.ShowInfo();
+
+internal class BaseKisi
+{
+    public string? Ad { get; init; }
+    public string? Soyad { get; init; }
+
+    public virtual void ShowInfo() // ayni isimdeki methodlar kullanacagimiz icin virtual olarak tanimladik.
+    {
+        Console.WriteLine($"Ad: {Ad ?? "Unknown"}, Soyad: {Soyad ?? "Unknown"}");
+    }
+}
+
+internal class Ogrenci : BaseKisi
+{
+    public int OgrenciNo { get; init; }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Ad: {Ad ?? "Unknown"}, Soyad: {Soyad ?? "Unknown"}, Ogrenci No: {OgrenciNo}");
+    }
+}
+
+internal class Ogretmen : BaseKisi
+{
+    public double Maas { get; init; }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Ad: {Ad ?? "Unknown"}, Soyad: {Soyad ?? "Unknown"}, Maas: {Maas}");
+    }
+}
