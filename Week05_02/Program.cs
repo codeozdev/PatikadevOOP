943df276e803536e04a9d40b4dde042c64139b5f
ozudev <ozudev@gmail.com>
2025-01-06 18:35:09 +0300

    Week 5 uygulama 2 (list)

---
+++ Week05_02/Program.cs
@@ -0,0 +1,9 @@
+﻿List<string> davetliler =
+    ["Bülent Ersoy", "Ajda Pekkan", "Ebru Gündeş", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın"];
+
+var index = 0;
+foreach (var davetli in davetliler)
+{
+    Console.WriteLine($"{index + 1} - {davetli}");
+    index++;
+}
