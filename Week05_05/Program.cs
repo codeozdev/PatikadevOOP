a4aac64506dc6d228a0a96341372d0bb827e6e88
ozudev <ozudev@gmail.com>
2025-01-07 12:44:21 +0300

    Week 5 uygulama 5 (try)

---
+++ Week05_05/Program.cs
@@ -0,0 +1,15 @@
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Lütfen bir sayı giriniz: ");
+        var number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Girdiğiniz sayının karesi: {number * number}");
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Girdiğiniz değer sayısal bir değer değil. Lütfen bir sayı giriniz.");
+    }
+}
